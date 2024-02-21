5ee030a7ae2eef75f8d12f8fb780f46e4b8713f8
Your Name <you@example.com>
2024-02-21 11:32:26 +0000

    Javascript complete
    need to edit html and css files

--- script.js
+++ script.js
@@ -1,76 +1,50 @@
-var options = {
-  startHour: 9,
-  endHour: 17,
-}
+$(document).ready(function() {
 
-function updateTimeslots() {
+  var timeDisplayEl = $('#time-display');
 
-  console.log('updateTimeslots');
-  var currentHour = dayjs().hour();
+  function displayTime() {
+    var rightNow = dayjs().format('dddd, DD MMM YYYY, hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+  
+  $('.saveBtn').on('click', function(){
+    var value = $(this).siblings('.description').val();
+    var hour = $(this).parent().attr('id');
 
-  $('.time-block').each(function (index, element) {
-    var hour = $(element).attr('data-hour');
-    console.log(hour, currentHour);
+    localStorage.setItem(hour, value);
 
-    if (hour < currentHour) {
-      $(element).find('.description').addClass('past');
-    }
-    else if (hour == currentHour) {
-      $(element).find('description').addClass('present');
-    }
-    else {
-      $(element).find('.description').addClass('future');
-    }
+    $('.notification').addClass('show');
+
+    setTimeout(function() {
+      $('.notification').removeClass('show');
+    }, 5000);
   });
-}
 
-  // Live clock and date //
+  $('.description').each(function() {
+    var data = $(this).parent().attr('id');
+    $(this).val(localStorage.getItem(data));
+  });
+  
+  function scheduleTime() {
+    var currentHour = dayjs().hour();
 
-const timeElement = document.getElementById("clock");
+    $('.time-block').each(function() {
+      var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
-function updateTime() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      }else if (blockHour == currentHour) {
+        $(this).addClass('present');
+      }else {
+        $(this).addClass('future');
+      }
+    });
+  }
 
-    const clockStart = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  $(document).ready(function () {
+     scheduleTime();
+  });
 
-    timeElement.innerText = clockStart;
-}
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = dd + '/' + mm + '/' + yyyy;
-document.write(today);
-
-updateTime();
-setInterval(updateTime, 1000);
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  displayTime();
+  setInterval(displayTime, 1000);
 });
